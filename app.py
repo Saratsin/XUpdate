c2f918712dc49acedea8c57149cf7a6063e2d870
Taras Shevchuk <schew4uk@gmail.com>
2017-07-10 15:34:31 +0300

    no message

--- app.py
+++ app.py
@@ -33,23 +33,23 @@ def wsgi_app():
     return bottle.default_app()
 
 if __name__ == '__main__':
-    app = bottle.Bottle()
-    srv = SSLWSGIRefServer(host="cefitbot.azurewebsites.net", port=80)
-    bottle.run(server=srv)
-    # PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-    # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static').replace('\\', '/')
-    # HOST = os.environ.get('SERVER_HOST', 'localhost')
-    # try:
-    #     PORT = int(os.environ.get('SERVER_PORT', '5555'))
-    # except ValueError:
-    #     PORT = 5555
-    #
-    # @bottle.route('/static/<filepath:path>')
-    # def server_static(filepath):
-    #     """Handler for static files, used with the development server.
-    #     When running under a production server such as IIS or Apache,
-    #     the server should be configured to serve the static files."""
-    #     return bottle.static_file(filepath, root=STATIC_ROOT)
-    #
-    # # Starts a local test server.
-    # bottle.run(server='wsgiref', host=HOST, port=PORT)
+    # app = bottle.Bottle()
+    # srv = SSLWSGIRefServer(host="cefitbot.azurewebsites.net", port=80)
+    # bottle.run(server=srv)
+    PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static').replace('\\', '/')
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+
+    @bottle.route('/static/<filepath:path>')
+    def server_static(filepath):
+        """Handler for static files, used with the development server.
+        When running under a production server such as IIS or Apache,
+        the server should be configured to serve the static files."""
+        return bottle.static_file(filepath, root=STATIC_ROOT)
+
+    # Starts a local test server.
+    bottle.run(server='wsgiref', host=HOST, port=PORT)
