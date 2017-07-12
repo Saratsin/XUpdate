407d0cb39a66e6ddf6bddf7b446250aa2fd79bad
Taras Shevchuk <schew4uk@gmail.com>
2017-07-12 12:26:27 +0300

    Bugfixes

--- routes.py
+++ routes.py
@@ -2,11 +2,13 @@
 Routes and views for the bottle application.
 """
 
-from bottle import route, view, redirect, request
+from bottle import route, view, redirect
+import bottle.request
+import urllib.request
 from datetime import datetime
 import telegram
 import json
-import urllib.request
+import app
 import ssl
 
 TOKEN = '387238739:AAHHtOlnJ2zL_BQ_KsbnlnX4NWqOXlzyFDA'
@@ -15,7 +17,7 @@ HOCKEYAPPTOKEN = 'eb31bf4e16804f768847185318d45c00'
 
 @route('/getApk')
 def getApp():
-    return redirect('https://rink.hockeyapp.net/api/2/apps/5678688052d344279b4f7dc00a203d3e/app_versions/{}?format=apk&avtoken=4c7da37fdb7681457730592e61afe7f3c38275a5'.format(app_id))
+    return redirect('https://rink.hockeyapp.net/api/2/apps/5678688052d344279b4f7dc00a203d3e/app_versions/{}?format=apk&avtoken=4c7da37fdb7681457730592e61afe7f3c38275a5'.format(app.app_id))
 
 @route('/checkForUpdate')
 def checkForUpdate():
@@ -54,7 +56,7 @@ def setWebhook():
 @route('/bothook', method='POST')
 def botHook():
     bot = telegram.Bot(TOKEN)
-    update = telegram.update.Update.de_json(request.json, bot)
+    update = telegram.update.Update.de_json(bottle.request.json, bot)
     bot.sendMessage(chat_id=update.message.chat_it, text=getSum(update.message.text, update.message.from_user.username))
     return 'OK'
 
